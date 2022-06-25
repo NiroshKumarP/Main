94034add3e3e3f2fe4bbed05f056748ba7fcd8ed
sures <sures@LAPTOP-8GGFC4AH>
2022-06-25 15:27:57 +0530

    june 23

--- src/GitHubOne.java
+++ src/GitHubOne.java
@@ -1,18 +1,15 @@
 
 public class GitHubOne {
-int a = 13;
-int b = 17;
-int totalAmount (int a,int b) {
-int totalAmount = a*b;
-return totalAmount;
+int a = 10;
+int b = 15;
+int totalAmount(int a,int b) {
+int totalAmount= (a*b);
 
-}
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-GitHubOne GHO = new GitHubOne ();
-int totalAmount =  GHO.totalAmount (13,17);
-System.out.println("the result:"+totalAmount);
-
-		
-	}	
+changes
 	}
+
+}
